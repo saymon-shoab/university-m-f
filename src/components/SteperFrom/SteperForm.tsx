37bec037e9c683e8704d798048c6c5babc8d1a8c
Saymon Shoab <76821250+saymon-shoab@users.noreply.github.com>
2024-08-27 17:18:05 +0600

    steper form create done

--- src/components/SteperFrom/SteperForm.tsx
+++ src/components/SteperFrom/SteperForm.tsx
@@ -1,27 +1,29 @@
-'use client'
-import React, { useState, useEffect } from 'react';
-import { Button, message, Steps } from 'antd';
-import { FormProvider, useForm } from 'react-hook-form';
-import { getFromLocalStorage, setToLocalStorage } from '@/utills/local_storage';
+"use client";
+import React, { useState, useEffect } from "react";
+import { Button, message, Steps } from "antd";
+import { FormProvider, useForm } from "react-hook-form";
+import { getFromLocalStorage, setToLocalStorage } from "@/utills/local_storage";
 
 interface ISteps {
-  title?: string,
+  title?: string;
   content?: React.ReactElement | React.ReactNode;
 }
 interface IStepsProps {
   steps: ISteps[];
   submitHandler: (er: any) => void;
-  navigateLink?: string
+  navigateLink?: string;
 }
 const SteperForm = ({ steps, submitHandler, navigateLink }: IStepsProps) => {
-  const router = useRouter()
+  const router = useRouter();
   const [current, setCurrent] = useState<number>(
-    !!getFromLocalStorage("step") ? Number(JSON.parse(getFromLocalStorage("step") as string).step) : 0
+    !!getFromLocalStorage("step")
+      ? Number(JSON.parse(getFromLocalStorage("step") as string).step)
+      : 0
   );
 
-  useEffect(()=>{
-     setToLocalStorage("step", JSON.stringify({step:current}))
-  },[current])
+  useEffect(() => {
+    setToLocalStorage("step", JSON.stringify({ step: current }));
+  }, [current]);
 
   const next = () => {
     setCurrent(current + 1);
@@ -33,40 +35,43 @@ const SteperForm = ({ steps, submitHandler, navigateLink }: IStepsProps) => {
 
   const items = steps.map((item) => ({ key: item.title, title: item.title }));
 
-  const methods = useForm()
-  const { handleSubmit, reset } = methods
+  const methods = useForm();
+  const { handleSubmit, reset } = methods;
   const handleSutdentOnSubmit = (data: any) => {
-    submitHandler(data)
-    reset()
-    setToLocalStorage("step", JSON.stringify({step:0}))
-    router.push(navigateLink)
-  }
+    submitHandler(data);
+    reset();
+    setToLocalStorage("step", JSON.stringify({ step: 0 }));
+    router.push(navigateLink);
+  };
   return (
     <>
       <Steps current={current} items={items} />
-      <FormProvider {...methods} >
+      <FormProvider {...methods}>
         <form onSubmit={handleSubmit(handleSutdentOnSubmit)}>
-          <div >{steps[current].content}</div>
-          <div >
+          <div>{steps[current].content}</div>
+          <div>
             {current < steps.length - 1 && (
               <Button type="primary" onClick={() => next()}>
                 Next
               </Button>
             )}
             {current === steps.length - 1 && (
-              <Button type="primary" htmlType="submit" onClick={() => message.success('Processing complete!')}>
+              <Button
+                type="primary"
+                htmlType="submit"
+                onClick={() => message.success("Processing complete!")}
+              >
                 Done
               </Button>
             )}
             {current > 0 && (
-              <Button style={{ margin: '0 8px' }} onClick={() => prev()}>
+              <Button style={{ margin: "0 8px" }} onClick={() => prev()}>
                 Previous
               </Button>
             )}
           </div>
         </form>
-      </FormProvider >
-
+      </FormProvider>
     </>
   );
 };
