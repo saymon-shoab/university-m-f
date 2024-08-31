73781ac7fceead4fdeebc463deed06009c4d2deb
Saymon Shoab <76821250+saymon-shoab@users.noreply.github.com>
2024-09-01 02:00:39 +0600

    create faculty code refactor

--- src/app/(withlayout)/super_admin/manage-faculty/create/page.tsx
+++ src/app/(withlayout)/super_admin/manage-faculty/create/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import FormDatePicker from "@/components/Forms/FormDatePicker";
 import FormInput from "@/components/Forms/FormInput";
 import Forms from "@/components/Forms/Form";
@@ -6,23 +6,135 @@ import FormSelectField from "@/components/Forms/FormSelectField";
 import FormTextArea from "@/components/Forms/FormTextArea";
 import UMBreadCrumb from "@/components/ui/UMBreadCrumb";
 import UploadImage from "@/components/ui/Uploadimage";
-import { acDepartmentOptions, acSemesterOptions, bloodGroupOptions, departmentOptions, facultyOptions, genderOptions } from "@/constants/global";
+import {
+  acDepartmentOptions,
+  acSemesterOptions,
+  bloodGroupOptions,
+  departmentOptions,
+  facultyOptions,
+  genderOptions,
+} from "@/constants/global";
 import { getUserInfo } from "@/service/auth.service";
 import { Button, Col, Row } from "antd";
 import Link from "next/link";
 import React from "react";
 
 const createFacultyPage = () => {
-
-  const onSubmit= async (data: any) => {
+  const facultyInformation = [
+    {
+      type: "text",
+      name: "faculty.name.firstName",
+      size: "large",
+      label: "First Name",
+    },
+    {
+      type: "text",
+      name: "faculty.name.middleName",
+      size: "large",
+      label: "Mid Name",
+    },
+    ,
+    {
+      type: "text",
+      name: "faculty.name.lastName",
+      size: "large",
+      label: "Last Name",
+    },
+    {
+      type: "password",
+      name: "password",
+      size: "large",
+      label: "Password",
+    },
+    {
+      type: "select",
+      size: "large",
+      name: "faculty.gender",
+      options: genderOptions,
+      label: "Gender",
+      placeholder: "Select",
+    },
+    {
+      type: "select",
+      name: "faculty.academicFaculty",
+      options: facultyOptions,
+      size: "large",
+      label: "Academic Faculry",
+      placeholder: "Select",
+    },
+    {
+      type: "select",
+      name: "faculty.academicDepartment",
+      options: acDepartmentOptions,
+      size: "large",
+      label: "Academic Faculry",
+      placeholder: "Select",
+    },
+    {
+      type: "file",
+      name: "file",
+    },
+  ];
+  const basicInformation = [
+    {
+      type: "email",
+      name: "faculty.email",
+      size: "large",
+      label: "Email address",
+    },
+    {
+      type: "text",
+      name: "faculty.contactNo",
+      size: "large",
+      label: "Contact No.",
+    },
+    {
+      type: "text",
+      name: "faculty.emergencyContactNo",
+      size: "large",
+      label: "Emergency Contact No.",
+    },
+    {
+      type: "date",
+      name: "faculty.dateOfBirth",
+      label: "Date of birth",
+      size: "large",
+    },
+    {
+      type: "select",
+      size: "large",
+      name: "faculty.bloodGroup",
+      options: bloodGroupOptions,
+      label: "Blood group",
+      placeholder: "Select",
+    },
+    {
+      type: "text",
+      name: "faculty.designation",
+      size: "large",
+      label: "Designation",
+    },
+    {
+      type: "textArea",
+      name: "faculty.presentAddress",
+      label: "Present address",
+      rows: 4,
+    },
+    {
+      type: "textArea",
+      name: "faculty.permanentAddress",
+      label: "Parmanent address",
+      rows: 4,
+    },
+  ]
+  const onSubmit = async (data: any) => {
     try {
       // const res = await
-      console.log(data)
-
+      console.log(data);
     } catch (err) {
-      console.error(err)
+      console.error(err);
     }
-  }
+  };
 
   const { role } = getUserInfo() as any;
   return (
@@ -30,8 +142,8 @@ const createFacultyPage = () => {
       <UMBreadCrumb
         items={[
           {
-            label: `${ role }`,
-            link: `${ role }`,
+            label: `${role}`,
+            link: `${role}`,
           },
           {
             label: "manage-faculty",
@@ -58,49 +170,35 @@ const createFacultyPage = () => {
             Faculty Information
           </p>
           <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
-        <Col className="gutter-row" style={{
-          marginBottom: "10px",
-        }} span={6}>
-          <FormInput name='student.name.firstName' type="text" size="large" label="First Name" />
-        </Col>
-        <Col className="gutter-row" style={{
-          marginBottom: "10px",
-        }} span={6}>
-          <FormInput name='student.name.midName' type="text" size="large" label="Mid Name" />
-        </Col>
-        <Col className="gutter-row" style={{
-          marginBottom: "10px",
-        }} span={6}>
-          <FormInput name='student.name.lastName' type="text" size="large" label="Last Name" />
-        </Col>
-        <Col className="gutter-row" style={{
-          marginBottom: "10px",
-        }} span={6}>
-          <FormInput name='password' type="password" size="large" label="Password" />
-        </Col>
-        <Col className="gutter-row" style={{
-          marginBottom: "10px",
-        }} span={8}>
-          <FormSelectField name='student.gender' options={genderOptions} size="large" label="Gender" placeholder="Select" />
-        </Col>
-        <Col className="gutter-row" style={{
-          marginBottom: "10px",
-        }} span={8}>
-          <FormSelectField name='student.academicFaculty' options={facultyOptions} size="large" label="Academic Faculry" placeholder="Select" />
-        </Col>
-        <Col className="gutter-row" style={{
-          marginBottom: "10px",
-        }} span={8}>
-          <FormSelectField name='student.academicDepartment' options={acDepartmentOptions} size="large" label="Academic Department" placeholder="Select" />
-        </Col>
-   
-     
-        <Col className="gutter-row" style={{
-          marginBottom: "10px",
-        }} span={8}>
-          <UploadImage name={"file"} />
-        </Col>
-      </Row>
+            {
+              facultyInformation.map((item:any)=>{
+                return (
+                  <Col
+                  className="gutter-row"
+                  style={{
+                    marginBottom: "10px",
+                  }}
+                  span={6}
+                >
+                  {
+                    item.type === "select" ?   <FormSelectField
+                    name={item.name}
+                    options={item.options}
+                    size={item.size}
+                    label={item.label}
+                    placeholder={item.placeholder}
+                  />: item.type ==="file"? <UploadImage name={item.file} />: <FormInput
+                  name={item.name}
+                  type={item.type}
+                  size={item.size}
+                  label={item.label}
+                  />
+                  }
+                </Col>
+                )
+              })
+            }
+          </Row>
         </div>
 
         {/* basic info */}
@@ -121,58 +219,52 @@ const createFacultyPage = () => {
             Basic Information
           </p>
           <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
-            <Col className="gutter-row" style={{
-              marginBottom: "10px",
-            }} span={8}>
-              <FormInput name='admin.email' type="email" size="large" label="Email" />
-            </Col>
-            <Col className="gutter-row" style={{
-              marginBottom: "10px",
-            }} span={8}>
-              <FormInput name='admin.ContactNo' type="number" size="large" label="Contant Number" />
-            </Col>
-            <Col className="gutter-row" style={{
-              marginBottom: "10px",
-            }} span={8}>
-              <FormInput name='admin.emergencyContactNo' type="number" size="large" label="Emargency Contact Number" />
-            </Col>
-            <Col className="gutter-row" style={{
-              marginBottom: "10px",
-            }} span={8}>
-              <FormDatePicker name="admin.dateOfBirth" label="Date Of Barth"/>
-            </Col>
-            <Col className="gutter-row" style={{
-              marginBottom: "10px",
-            }} span={8}>
-              <FormSelectField name='admin.bloodGroup' options={bloodGroupOptions} size="large" label="Blood Group" placeholder="Select" />
-            </Col>
-            <Col className="gutter-row" style={{
-              marginBottom: "10px",
-            }} span={8}>
-              <FormInput name='admin.designation' type="text"  size="large" label="Designation" />
-            </Col>
-            <Col span={12} style={{ margin: "10px 0" }}>
-                <FormTextArea
-                  name="admin.presentAddress"
-                  label="Present address"
-                  rows={4}
-                />
-              </Col>
-
-              <Col span={12} style={{ margin: "10px 0" }}>
-                <FormTextArea
-                  name="admin.permanentAddress"
-                  label="Permanent address"
-                  rows={4}
-                />
-              </Col>
+          {basicInformation.map((item: any) => {
+                return (
+                  <Col
+                    className="gutter-row"
+                    span={8}
+                    style={{
+                      marginBottom: "10px",
+                    }}
+                  >
+                    {item.type === "select" ? (
+                      <FormSelectField
+                        size={item.size}
+                        name={item.name}
+                        options={item.options}
+                        label={item.label}
+                        placeholder={item.select}
+                      />
+                    ) : item.type === "date" ? (
+                      <FormDatePicker
+                        name={item.name}
+                        label={item.label}
+                        size={item.size}
+                      />
+                    ) : item.type === "textArea" ? (
+                      <FormTextArea
+                        name={item.name}
+                        label={item.label}
+                        rows={item.rows}
+                      />
+                    ) : (
+                      <FormInput
+                        name={item.name}
+                        type={item.type}
+                        size={item.size}
+                        label={item.label}
+                      />
+                    )}
+                  </Col>
+                );
+              })}
           </Row>
         </div>
         <Button htmlType="submit" type="primary">
           Create Admin
         </Button>
       </Forms>
-      
     </div>
   );
 };
